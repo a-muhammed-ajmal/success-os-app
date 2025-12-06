c3aea841535f11cefe60b8163d39122456320176
a-muhammed-ajmal <ajmalconsults@gmail.com>
2025-12-06 05:28:58 +0400

    Fix Tailwind CSS version pinning and resolve update issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Layout from './components/layout/Layout';
-import Dashboard from './pages/Dashboard';
-import Finance from './pages/Finance';
-import Priorities from './pages/Priorities';
-import Professional from './pages/Professional';
-import Relationship from './pages/Relationship';
-import Reviews from './pages/Reviews';
-import Visions from './pages/Visions';
-import Wellness from './pages/Wellness';
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/layout/Layout'
+import Dashboard from './pages/Dashboard'
+import Finance from './pages/Finance'
+import Priorities from './pages/Priorities'
+import Professional from './pages/Professional'
+import Relationship from './pages/Relationship'
+import Reviews from './pages/Reviews'
+import Visions from './pages/Visions'
+import Wellness from './pages/Wellness'
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/professional" element={<Professional />} />
-          <Route path="/finance" element={<Finance />} />
-          <Route path="/priorities" element={<Priorities />} />
-          <Route path="/wellness" element={<Wellness />} />
-          <Route path="/relationship" element={<Relationship />} />
-          <Route path="/visions" element={<Visions />} />
-          <Route path="/reviews" element={<Reviews />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  );
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="priorities" element={<Priorities />} />
+        <Route path="professional" element={<Professional />} />
+        <Route path="finance" element={<Finance />} />
+        <Route path="relationship" element={<Relationship />} />
+        <Route path="reviews" element={<Reviews />} />
+        <Route path="visions" element={<Visions />} />
+        <Route path="wellness" element={<Wellness />} />
+      </Route>
+    </Routes>
+  )
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { initDB } from './lib/db.js'
-
-initDB();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
